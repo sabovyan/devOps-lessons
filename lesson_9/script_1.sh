4fda4b9b706464319c476a84f4c8974d0ba66c4d
sabovyan <abovyan.sargis90@gmail.com>
2022-10-22 01:46:47 +0000

    [lesson_9]: add devision task

---
+++ lesson_9/script_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "enter a number"
+
+read -r number
+
+devidedBy3=$(("$number" % 3))
+devidedBy5=$(("$number" % 5))
+devidedBy7=$(("$number" % 7))
+
+if [[ "$devidedBy3" -eq 0 || "$devidedBy7" -eq 0 || "$devidedBy5" -eq 0 ]]
+ then
+  echo "divisible"
+else 
+  echo "not devisible"
+fi
