7821bffc3017d3848235a643f13dc64278d27378
sabovyan <abovyan.sargis90@gmail.com>
2023-02-03 21:13:01 +0400

    add security group

--- terraform_lesson/ELB/main.tf
+++ terraform_lesson/ELB/main.tf
@@ -29,13 +29,72 @@ data "aws_ami" "amazon-linux-2" {
 }
 
 resource "aws_instance" "my_instance_1" {
-  ami           = data.aws_ami.amazon-linux-2.id
-  instance_type = "t2.micro"
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.wizard_3.id]
+
+  tags = {
+    "Name" = "my_instance_1"
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+EOF
 
 }
 
 resource "aws_instance" "my_instance_2" {
-  ami           = data.aws_ami.amazon-linux-2.id
-  instance_type = "t2.micro"
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.wizard_3.id]
 
+  user_data = <<EOF
+#!/bin/bash
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+EOF
+
+  tags = {
+    "Name" = "my_instance_2"
+  }
+
+}
+
+
+resource "aws_security_group" "wizard_3" {
+  name        = "wizard_3"
+  description = "Allow HTTP and SSH traffic via Terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
